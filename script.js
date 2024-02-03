67f683ed7c77fdb594440aeb3388ba07d0833e34
TheBareMinimum <158708141+TheBareMinimum@users.noreply.github.com>
2024-02-03 13:45:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function showMessage(response) {
   const responseDiv = document.getElementById('response');
   if (response === 'No') {
     responseDiv.innerHTML = 'Aw, really? Please reconsider!';
-  } else {
+  } else if (response === 'Yes') {
     responseDiv.innerHTML = 'Great! Looking forward to it!';
+  } else {
+    responseDiv.innerHTML = 'Invalid response. Please click Yes or No.';
   }
 }
