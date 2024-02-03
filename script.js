fd41d3d783a00c32099113cd78867031ff6505df
TheBareMinimum <158708141+TheBareMinimum@users.noreply.github.com>
2024-02-03 13:22:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage(response) {
+  const responseDiv = document.getElementById('response');
+  if (response === 'No') {
+    responseDiv.innerHTML = 'Aw, really? Please reconsider loduuu;
+  } else {
+    responseDiv.innerHTML = 'Great! Looking forward to it and shapghet w shroom bals!’;
+  }
+}
